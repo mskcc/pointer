2b6cff0cc1ed7c554fc5b50477a487fa87005224
Nikhil <nikhilkumar516@gmail.com>
2020-08-24 19:59:34 -0400

    Updated babel configuration to handle warnings

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@ module.exports = function (api) {
   api.cache(true);
 
   const plugins = [['@babel/transform-runtime', { regenerator: true }]];
-  const presets = ['@babel/preset-react', '@babel/preset-env'];
+  const presets = ['@babel/preset-react', ['@babel/preset-env', {modules: 'commonjs',}]];
 
   return {
     presets,
