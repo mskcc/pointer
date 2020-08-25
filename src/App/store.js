1000ccfc6f7ea5c6ff35219cb0c05fb9a2029f4f
Nikhil <nikhilkumar516@gmail.com>
2020-08-24 20:03:14 -0400

    Slight fix and formatting

--- src/App/store.js
+++ src/App/store.js
@@ -1,34 +1,32 @@
-import { createStore, applyMiddleware } from 'redux';
-import { Router, Route, browserHistory } from 'react-router'
-import { syncHistoryWithStore, routerReducer, routerMiddleware, push } from 'react-router-redux'
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Router, Route, browserHistory } from 'react-router';
+import { syncHistoryWithStore, routerReducer, routerMiddleware, push } from 'react-router-redux';
 
-import { createLogger } from "redux-logger"
-import thunk from "redux-thunk"
-import promise from "redux-promise-middleware"
+import { createLogger } from 'redux-logger';
+import thunk from 'redux-thunk';
+import promise from 'redux-promise-middleware';
 
-import reducer from "./root_reducer.js"
+import reducer from './root_reducer.js';
 
 import { history } from '@/_helpers';
 import { currentUserSubject } from '@/_services';
 
 const routeMiddleware = routerMiddleware(browserHistory);
 
-
 const authInterceptor = ({ dispatch }) => (next) => (action) => {
-    if (action.status === 401) {
+    if (action.status === 401 || action.status === 403) {
         localStorage.removeItem('currentUser');
         currentUserSubject.next(null);
-        history.push(`/login`)
+        history.push(`/login`);
         // dispatch(actions.removeJwt());
     } else {
         next(action);
     }
 };
 
-
 const middleware = applyMiddleware(thunk, createLogger(), routeMiddleware, authInterceptor);
 
 // Redux Devtools config
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-export default createStore(reducer, {},  composeEnhancers(middleware));
+export default createStore(reducer, {}, composeEnhancers(middleware));
