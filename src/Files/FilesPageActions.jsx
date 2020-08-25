907049de353eb85f1b62cd0e4bf8f5adc744c4da
Nikhil <nikhilkumar516@gmail.com>
2020-08-24 20:06:00 -0400

    Added status value on action

--- src/Files/FilesPageActions.jsx
+++ src/Files/FilesPageActions.jsx
@@ -1,20 +1,18 @@
-import axios from 'axios'
+import axios from 'axios';
 
 import {
     API_URL,
     FILES_ENDPOINT,
-
     FETCH_FILES_LIST,
     FILES_LIST_FULFILLED,
     FILES_LIST_ERROR,
-} from '../constants'
+} from '../constants';
 
 import { authHeader } from '@/_helpers';
 
-
 export function loadFilesList(page, file_group, file_type, metadata, filename, filename_regex) {
     return function (dispatch) {
-        dispatch({type: FETCH_FILES_LIST});
+        dispatch({ type: FETCH_FILES_LIST });
 
         let params = {
             page: unescape(page),
@@ -36,13 +34,16 @@ export function loadFilesList(page, file_group, file_type, metadata, filename, f
             params.filename_regex = filename_regex;
         }
 
-        axios.get(API_URL + FILES_ENDPOINT, {
-            params: params,
-            headers: authHeader()
-        }).then((resp) => {
-            dispatch({type: FILES_LIST_FULFILLED, payload: resp.data});
-        }).catch((err) => {
-            dispatch({type: FILES_LIST_ERROR, payload: err});
-        });
-    }
+        axios
+            .get(API_URL + FILES_ENDPOINT, {
+                params: params,
+                headers: authHeader(),
+            })
+            .then((resp) => {
+                dispatch({ type: FILES_LIST_FULFILLED, payload: resp.data });
+            })
+            .catch((err) => {
+                dispatch({ type: FILES_LIST_ERROR, payload: err, status: err.response.status });
+            });
+    };
 }
--- src/Run/RunsPageActions.jsx
+++ src/Run/RunsPageActions.jsx
@@ -1,70 +1,70 @@
-import axios from 'axios'
+import axios from 'axios';
 
 import {
     API_URL,
     RUNS_ENDPOINT,
     PORTS_ENDPOINT,
-
     FETCH_RUNS_LIST,
     FETCH_RUNS_FULFILLED,
     FETCH_RUNS_ERROR,
-
     START_CREATE_RUN,
     CREATE_RUN_FULFILLED,
     CREATE_RUN_ERROR,
-
     FETCH_RUN,
     FETCH_RUN_FULFILLED,
     FETCH_RUN_ERROR,
-
     START_UPDATE_PORT,
     UPDATE_PORT_FULFILLED,
     UPDATE_PORT_ERROR,
-
     UPDATE_RUN,
     UPDATE_RUN_FULFILLED,
     UPDATE_RUN_ERROR,
-} from '../constants'
+} from '../constants';
 
 import { authHeader } from '@/_helpers';
 
-
 export function getRuns(page) {
     return function (dispatch) {
-        dispatch({type: FETCH_RUNS_LIST});
-        axios.get(API_URL + RUNS_ENDPOINT, {
-            params: {
-                page: unescape(page),
-            },
-            headers: authHeader()
-        }).then((resp) => {
-            dispatch({type: FETCH_RUNS_FULFILLED, payload: resp.data});
-        }).catch((err) => {
-            dispatch({type: FETCH_RUNS_ERROR, payload: err});
-        });
-    }
+        dispatch({ type: FETCH_RUNS_LIST });
+        axios
+            .get(API_URL + RUNS_ENDPOINT, {
+                params: {
+                    page: unescape(page),
+                },
+                headers: authHeader(),
+            })
+            .then((resp) => {
+                dispatch({ type: FETCH_RUNS_FULFILLED, payload: resp.data });
+            })
+            .catch((err) => {
+                dispatch({ type: FETCH_RUNS_ERROR, payload: err, status: err.response.status });
+            });
+    };
 }
 
 export function getRun(run_id) {
     return function (dispatch) {
-        dispatch({type: FETCH_RUN});
+        dispatch({ type: FETCH_RUN });
 
-        return axios.get(API_URL + RUNS_ENDPOINT, {
-            params: {
-                run_id: unescape(run_id),
-            },
-            headers: authHeader(),
-        }).then((resp) => {
-            dispatch({type: FETCH_RUN_FULFILLED, payload: resp.data});
-        }).catch((err) => {
-            dispatch({type: FETCH_RUN_ERROR, payload: err});
-        });
+        return axios
+            .get(API_URL + RUNS_ENDPOINT, {
+                params: {
+                    run_id: unescape(run_id),
+                },
+                headers: authHeader(),
+            })
+            .then((resp) => {
+                dispatch({ type: FETCH_RUN_FULFILLED, payload: resp.data });
+            })
+            .catch((err) => {
+                dispatch({ type: FETCH_RUN_ERROR, payload: err, status: err.response.status });
+            });
     };
 }
 
 export function createRun(pipeline_id, request_id) {
     return function (dispatch) {
-        dispatch({type: START_CREATE_RUN});
+        dispatch({ type: START_CREATE_RUN });
 
         return axios(API_URL + RUNS_ENDPOINT, {
             method: 'post',
@@ -72,54 +72,65 @@ export function createRun(pipeline_id, request_id) {
             data: {
                 pipeline_id: pipeline_id,
                 request_id: request_id,
-            }
-        }).then((resp) => {
-            dispatch({type: CREATE_RUN_FULFILLED, payload: resp.data});
-        }).catch((err) => {
-            dispatch({type: CREATE_RUN_ERROR, payload: err});
-        });
+            },
+        })
+            .then((resp) => {
+                dispatch({ type: CREATE_RUN_FULFILLED, payload: resp.data });
+            })
+            .catch((err) => {
+                dispatch({ type: CREATE_RUN_ERROR, payload: err, status: err.response.status });
+            });
     };
-
 }
 
 export function updatePorts(run_id, inputs, status) {
     return function (dispatch) {
         for (const key in inputs) {
-            dispatch({type: START_UPDATE_PORT});
+            dispatch({ type: START_UPDATE_PORT });
 
-            axios.put(API_URL + PORTS_ENDPOINT, {
-                params: {
-                    run_id: unescape(run_id),
-                },
-                headers: authHeader(),
-                body: JSON.stringify({values: inputs[key]})
-            }).then((resp) => {
-                dispatch({type: UPDATE_PORT_FULFILLED, payload: resp.data});
-                // Need to automatically update runs after updating ports
-                // Todo: why?
-                dispatch({
-                    type: UPDATE_RUN,
-                    id: id,
-                    payload: resp.data
+            axios
+                .put(API_URL + PORTS_ENDPOINT, {
+                    params: {
+                        run_id: unescape(run_id),
+                    },
+                    headers: authHeader(),
+                    body: JSON.stringify({ values: inputs[key] }),
+                })
+                .then((resp) => {
+                    dispatch({ type: UPDATE_PORT_FULFILLED, payload: resp.data });
+                    // Need to automatically update runs after updating ports
+                    // Todo: why?
+                    dispatch({
+                        type: UPDATE_RUN,
+                        id: id,
+                        payload: resp.data,
+                    });
+                })
+                .catch((err) => {
+                    dispatch({
+                        type: UPDATE_PORT_ERROR,
+                        payload: err,
+                        status: err.response.status,
+                    });
                 });
-            }).catch((err) => {
-                dispatch({type: UPDATE_PORT_ERROR, payload: err});
-            });
         }
     };
 }
 
 export function updateRun(id, status) {
     return function (dispatch) {
-        return axios.put(API_URL + RUNS_ENDPOINT + '/${id}', {
-            params: {
-                run_id: unescape(id),
-            },
-            headers: authHeader(),
-        }).then((resp) => {
-            dispatch({type: UPDATE_RUN_FULFILLED, payload: resp.data});
-        }).catch((err) => {
-            dispatch({type: UPDATE_RUN_ERROR, payload: err});
-        });
+        return axios
+            .put(API_URL + RUNS_ENDPOINT + '/${id}', {
+                params: {
+                    run_id: unescape(id),
+                },
+                headers: authHeader(),
+            })
+            .then((resp) => {
+                dispatch({ type: UPDATE_RUN_FULFILLED, payload: resp.data });
+            })
+            .catch((err) => {
+                dispatch({ type: UPDATE_RUN_ERROR, payload: err, status: err.response.status });
+            });
     };
 }
