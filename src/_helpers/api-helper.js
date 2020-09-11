68e9294fca87b943ea1b4fc575ebd153641fe93f
Nikhil <nikhilkumar516@gmail.com>
2020-09-11 18:57:33 -0400

    Added api helper

---
+++ src/_helpers/api-helper.js
@@ -0,0 +1,37 @@
+import { SERVER_DOWN } from '../constants';
+// Helper functions for api calls
+export function handleError(err) {
+    let data = {};
+    let status = null;
+    if (!err.response) {
+        data = {
+            detail: SERVER_DOWN,
+        };
+        status = 503;
+    } else {
+        data = err.response.data;
+        status = err.response.status;
+    }
+    return { data: data, status: status };
+}
+
+export function handleSingleParam(params, name, value) {
+    if (value != null && value !== '') {
+        params[name] = decodeURI(value);
+    }
+}
+
+export function handleSingleBoolParam(params, name, value) {
+    if (value != null && value !== '') {
+        params[name] = true;
+    }
+}
+
+export function handleArrayParam(params, name, valueList) {
+    if (Array.isArray(valueList) && valueList.length) {
+        params[name] = [];
+        for (const singleValue of valueList) {
+            params[name].push(singleValue);
+        }
+    }
+}
