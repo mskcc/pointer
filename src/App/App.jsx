638ec076dd45ef3793ff6fb14c708e2c8668c686
Nikhil <nikhilkumar516@gmail.com>
2020-09-11 19:01:48 -0400

    Updated App to include navigation

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -3,8 +3,33 @@ import React from 'react';
 import '@/App/App.css';
 import * as appActions from './AppActions';
 
-import { bindActionCreators } from 'redux';
+import { withStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { render } from 'react-dom';
+import { Router, Route, Link, Redirect, Switch } from 'react-router-dom';
 import { Provider, connect } from 'react-redux';
+
+import { PrivateRoute } from '@/_components';
+import { StartRun } from '@/Run/StartRun';
+import { Unauthorized } from '@/Unauthorized';
+import ConnectedFilePage from '@/Files/File';
+import FilesPage from '@/Files/FilesPage';
+import ConnectedRunsPage from '@/Run/RunsPage.jsx';
+import ConnectedLoginPage from '@/LoginPage/LoginPage';
+import ConnectedLogoutPage from '@/LogoutPage/LogoutPage';
+import ConnectedPipelinePage from '@/PipelinePage/PipelinePage';
+import SummaryPage from '@/Summary/SummaryPage';
+import MetadataPage from '@/Metadata/MetadataPage';
+import Drawer from '@/_components/Drawer';
+import '@/App/App.css';
+import mskLogo from '@/public/MSKCC-logo.jpg';
+import Grid from '@material-ui/core/Grid';
+
+import store from '@/App/store.js';
+import { history } from '@/_helpers';
+import { authenticationService } from '@/_services';
+
+import { bindActionCreators } from 'redux';
 
 const mapStateToProps = function (state) {
     return {};
@@ -14,16 +39,95 @@ const mapDispatchToProps = function (dispatch) {
     return bindActionCreators(appActions, dispatch);
 };
 
-class App extends React.Component {
+const useStyles = (theme) => ({
+    root: {
+        display: 'flex',
+    },
+    // necessary for content to be below app bar
+    toolbar: theme.mixins.toolbar,
+    content: {
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.default,
+        padding: theme.spacing(3),
+    },
+});
+
+class AppContainer extends React.Component {
     constructor(props) {
         super(props);
+
+        const pages = [
+            { link: '/summary', icon: 'insert_chart', key: 'summary', text: 'Summary' },
+            { link: '/runs', icon: 'clear_all', key: 'runs', text: 'Runs' },
+            { link: '/files', icon: 'source', key: 'files', text: 'Files' },
+            { link: '/pipelines', icon: 'tune', key: 'Pipelines', text: 'Pipelines' },
+            { link: '/logout', icon: 'logout', key: 'logout', text: 'Logout' },
+            { link: '/metadata', icon: 'local_offer', key: 'metadata', text: 'Metadata' },
+        ];
+        this.state = {
+            currentUser: null,
+            pages: pages,
+        };
     }
 
-    componentDidMount() {}
+    componentDidMount() {
+        authenticationService.currentUser.subscribe((userObj) => {
+            this.setState({
+                currentUser: userObj,
+            });
+            if (!(userObj && userObj.user)) {
+                history.push('/login');
+            }
+        });
+    }
+
+    logout() {
+        authenticationService.logout();
+        history.push('/login');
+    }
 
     render() {
-        return <div id="app__container">{this.props.children}</div>;
+        const { currentUser, pages } = this.state;
+        const { classes } = this.props;
+        let email = '';
+        if (currentUser && currentUser.user) {
+            email = currentUser.user.email;
+        }
+
+        return (
+            <Router history={history}>
+                <div className={classes.root}>
+                    <CssBaseline />
+                    <Route
+                        path={[
+                            '/files',
+                            '/file/:id',
+                            '/run/:id',
+                            '/pipelines',
+                            '/runs',
+                            '/summary',
+                            '/metadata',
+                        ]}
+                        render={() => <Drawer user={email} pages={pages}></Drawer>}
+                    />
+                    <div className={`jumbotron ${classes.content}`}>
+                        <Switch>
+                            <Route path="/files" component={FilesPage} />
+                            <Route path="/file/:id" component={ConnectedFilePage} />
+                            <Route path="/run/:id" component={StartRun} />
+                            <Route path="/login" component={ConnectedLoginPage} />
+                            <Route path="/logout" component={ConnectedLogoutPage} />
+                            <Route path="/pipelines" component={ConnectedPipelinePage} />
+                            <Route path="/runs" component={ConnectedRunsPage} />
+                            <Route path="/summary" component={SummaryPage} />
+                            <Route path="/metadata" component={MetadataPage} />
+                            <Route path="/" component={ConnectedLoginPage} />
+                            <Route component={Unauthorized} />
+                        </Switch>
+                    </div>
+                </div>
+            </Router>
+        );
     }
 }
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(withStyles(useStyles)(AppContainer));
